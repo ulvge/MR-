46b3c94d60c410a44dd5de8d9b8cbd5e769907d4
zhoubin <zhoubin@qq.com>
2023-05-16 10:47:19 +0800

    Add: CPLD get all BANK class by pin name

--- Debug/CPLDTable.cs
+++ Debug/CPLDTable.cs
@@ -484,21 +484,137 @@ namespace Debug
 			public string pin { get; set; } // eg: -10, 20
 			public string IOSTANDARD { get; set; }
 			public V2_OriDesc(string humanName, string pin) {
-				this.humanName = humanName;
+				this.humanName = humanName.Replace("M.2", "M_2");
 				this.pin = pin;
-				this.IOSTANDARD = JugeIOS(pin);
+				this.IOSTANDARD = GetBankVolLevel(pin);
 			}
-			private string JugeIOS(string pin) {
-				char row = pin[0];
+			private class LVCMOS {
+				public string rowName;
+				public int min;
+				public int max;
+
+				public LVCMOS(string rowName, int min, int max) {
+					this.rowName = rowName;
+					this.min = min;
+					this.max = max;
+				}
+			}
+			private class BankVolotLevel {
+				public static string BankVolotLevel_1P8 = "LVCMOS18";
+				public static string BankVolotLevel_3P3 = "LVCMOS33";
+			}
+			private class BankClass {
+				public string bankName;
+				public string bankVolotLevel;
+				public LVCMOS[] range;
+                    
+                public BankClass(string bankName, string bankVolotLevel, LVCMOS[] range_Bank) {
+                    this.bankName = bankName;
+                    this.bankVolotLevel = bankVolotLevel;
+                    this.range = range_Bank;
+				}
+				public static LVCMOS[] Range_Bank0 = {
+					new LVCMOS("A", 3, 15),
+					new LVCMOS("B", 3, 14),
+					new LVCMOS("C", 4, 13),
+					new LVCMOS("D", 5, 12),
+					new LVCMOS("E", 6, 11),
+					new LVCMOS("F", 7, 10),
+				};
+				public static LVCMOS[] Range_Bank1 = {
+					new LVCMOS("B", 16, 16),
+					new LVCMOS("C", 15, 16),
+					new LVCMOS("D", 14, 16),
+					new LVCMOS("E", 13, 16),
+					new LVCMOS("F", 12, 16),
+
+					new LVCMOS("G", 11, 16),
+					new LVCMOS("H", 10, 16),
+					new LVCMOS("J", 10, 16),
+					new LVCMOS("K", 11, 16),
+
+					new LVCMOS("L", 12, 16),
+					new LVCMOS("M", 13, 13),
+					new LVCMOS("M", 16, 16),
+				};
+				public static LVCMOS[] Range_Bank2 = {
+					new LVCMOS("L", 7, 10),
+					new LVCMOS("M", 6, 11),
+					new LVCMOS("M", 14, 15),
+					new LVCMOS("N", 6, 16),
+					new LVCMOS("P", 4, 16),
+					new LVCMOS("R", 3, 16),
+					new LVCMOS("T", 2, 16),
+				};
+				public static LVCMOS[] Range_Bank3 = {
+					new LVCMOS("K", 4, 5),
+					new LVCMOS("L", 1, 5),
+					new LVCMOS("M", 1, 4),
+					new LVCMOS("N", 1, 3),
+					new LVCMOS("P", 1, 2),
+					new LVCMOS("R", 1, 1),
+				};
+				public static LVCMOS[] Range_Bank4 = {
+					new LVCMOS("G", 1, 1),
+					new LVCMOS("H", 1, 5),
+					new LVCMOS("H", 7, 7),
+					new LVCMOS("J", 1, 7),
+					new LVCMOS("K", 1, 3),
+					new LVCMOS("K", 6, 6),
+				};
+				public static LVCMOS[] Range_Bank5 = {
+					new LVCMOS("B", 1, 1),
+					new LVCMOS("C", 1, 2),
+					new LVCMOS("D", 1, 3),
+					new LVCMOS("E", 1, 4),
+					new LVCMOS("F", 1, 5),
+					new LVCMOS("G", 2, 6),
+					new LVCMOS("H", 6, 6),
+				};
+			};
+			private BankClass[] g_BankClass =  {
+				new BankClass("bank0", BankVolotLevel.BankVolotLevel_3P3, BankClass.Range_Bank0),
+				new BankClass("bank1", BankVolotLevel.BankVolotLevel_3P3, BankClass.Range_Bank1),
+				new BankClass("bank2", BankVolotLevel.BankVolotLevel_3P3, BankClass.Range_Bank2),
+				new BankClass("bank3", BankVolotLevel.BankVolotLevel_1P8, BankClass.Range_Bank3),
+				new BankClass("bank4", BankVolotLevel.BankVolotLevel_1P8, BankClass.Range_Bank4),
+				new BankClass("bank5", BankVolotLevel.BankVolotLevel_1P8, BankClass.Range_Bank5),
+			};
+			private string GetBankVolLevel(string pin) {
+				if(pin.Equals("T6")) {
+					Console.WriteLine(pin);
+                }
+				string row = pin.Substring(0,1);
 				int column = int.Parse(pin.Substring(1));
-				if (row >= 'L') {
-					int rowNum = row - 'L';
-					if((rowNum + column >= 7) && (rowNum + column <= 10)) {
-						return "LVCMOS18";
+
+                foreach(var bank in g_BankClass) {
+					LVCMOS[] range = bank.range;
+                    foreach(var item in range) {
+						if(item.rowName == row) {
+							if((column >= item.min) && (column <= item.max)) {
+								return bank.bankVolotLevel;
+							}
+						}
 					}
                 }
-				return "LVCMOS33";
+				return "LVCMOSERROR";
 			}
+		}
+
+		private string[] StringCommit = {
+				"JTAG_",
+				"PROGRAMN",
+			};
+		private bool IsPinNameNeedCommit(string pinName) {
+			if(string.IsNullOrEmpty(pinName)) {
+				return true;
+			}
+			foreach(var item in StringCommit) {
+				if(pinName.Contains(item)) {
+					return true;
+				}
+			}
+			return false;
 		}
 		string oriFile = "../../res/ori.txt";
 		string destFile = "../../res/dest.txt";
@@ -536,7 +652,12 @@ namespace Debug
 			StringBuilder sb = new StringBuilder();
             foreach(var item in destDesc) {
 				//set_pin_assignment { CPU_CKOBV_SEL0 } {  LOCATION = T6   ;IOSTANDARD = LVCMOS18  ;PULLTYPE = NONE; }
-				sb.Append("set_pin_assignment { " + item.humanName + " } { LOCATION = " + item.pin + "; IOSTANDARD = " + item.IOSTANDARD + "; PULLTYPE = NONE; }\r\n");
+				string assembly = ("set_pin_assignment { " + item.humanName + " } { LOCATION = " + item.pin + "; IOSTANDARD = " + item.IOSTANDARD + "; PULLTYPE = NONE; }\r\n");
+				if(IsPinNameNeedCommit(item.humanName)) {
+					sb.Insert(0, "#" + assembly);
+                } else {
+					sb.Append(assembly);
+				}
 			}
 			File.WriteAllText(destFile, sb.ToString());
 		}
